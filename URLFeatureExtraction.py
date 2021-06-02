6bf90f13edc2746e6f4b35722fecb309d9dd13a4
Snehal Patil <51523547+snehalpatil4@users.noreply.github.com>
2021-06-02 14:57:00 +0530

    Add files via upload

--- URLFeatureExtraction.py
+++ URLFeatureExtraction.py
@@ -1,29 +1,9 @@
 # -*- coding: utf-8 -*-
 
-# importing required packages for this section
+
 from urllib.parse import urlparse,urlencode
 import ipaddress
-import re
 
-"""#### **3.1.1. Domain of the URL**
-Here, we are just extracting the domain present in the URL. This feature doesn't have much significance in the training. May even be dropped while training the model.
-"""
-'''
-# 1.Domain of the URL (Domain) 
-def getDomain(url):  
-  domain = urlparse(url).netloc
-  if re.match(r"^www.",domain):
-	       domain = domain.replace("www.","")
-  return domain'''
-
-"""#### **3.1.2. IP Address in the URL**
-
-Checks for the presence of IP address in the URL. URLs may have IP address instead of domain name. If an IP address is used as an alternative of the domain name in the URL, we can be sure that someone is trying to steal personal information with this URL.
-
-If the domain part of URL has IP address, the value assigned to this feature is 1 (phishing) or else 0 (legitimate).
-"""
-
-# 2.Checks for IP address in URL (Have_IP)
 def havingIP(url):
   try:
     ipaddress.ip_address(url)
@@ -32,12 +12,7 @@ def havingIP(url):
     ip = 0
   return ip
 
-"""#### **3.1.3. "@" Symbol in URL**
 
-Checks for the presence of '@' symbol in the URL. Using “@” symbol in the URL leads the browser to ignore everything preceding the “@” symbol and the real address often follows the “@” symbol. 
-
-If the URL has '@' symbol, the value assigned to this feature is 1 (phishing) or else 0 (legitimate).
-"""
 
 # 3.Checks the presence of @ in URL (Have_At)
 def haveAtSign(url):
@@ -47,12 +22,6 @@ def haveAtSign(url):
     at = 0    
   return at
 
-"""#### **3.1.4. Length of URL**
-
-Computes the length of the URL. Phishers can use long URL to hide the doubtful part in the address bar. In this project, if the length of the URL is greater than or equal 54 characters then the URL classified as phishing otherwise legitimate.
-
-If the length of URL >= 54 , the value assigned to this feature is 1 (phishing) or else 0 (legitimate).
-"""
 
 # 4.Finding the length of URL and categorizing (URL_Length)
 def getLength(url):
@@ -62,12 +31,6 @@ def getLength(url):
     length = 1            
   return length
 
-"""#### **3.1.5. Depth of URL**
-
-Computes the depth of the URL. This feature calculates the number of sub pages in the given url based on the '/'.
-
-The value of feature is a numerical based on the URL.
-"""
 
 # 5.Gives number of '/' in URL (URL_Depth)
 def getDepth(url):
@@ -78,12 +41,6 @@ def getDepth(url):
       depth = depth+1
   return depth
 
-"""#### **3.1.6. Redirection "//" in URL**
-
-Checks the presence of "//" in the URL. The existence of “//” within the URL path means that the user will be redirected to another website. The location of the “//” in URL is computed. We find that if the URL starts with “HTTP”, that means the “//” should appear in the sixth position. However, if the URL employs “HTTPS” then the “//” should appear in seventh position.
-
-If the "//" is anywhere in the URL apart from after the protocal, thee value assigned to this feature is 1 (phishing) or else 0 (legitimate).
-"""
 
 # 6.Checking for redirection '//' in the url (Redirection)
 def redirection(url):
@@ -96,12 +53,6 @@ def redirection(url):
   else:
     return 0
 
-"""#### **3.1.7. "http/https" in Domain name**
-
-Checks for the presence of "http/https" in the domain part of the URL. The phishers may add the “HTTPS” token to the domain part of a URL in order to trick users.
-
-If the URL has "http/https" in the domain part, the value assigned to this feature is 1 (phishing) or else 0 (legitimate).
-"""
 
 # 7.Existence of “HTTPS” Token in the Domain Part of the URL (https_Domain)
 def httpDomain(url):
@@ -111,12 +62,7 @@ def httpDomain(url):
   else:
     return 0
 
-"""#### **3.1.8. Using URL Shortening Services “TinyURL”**
 
-URL shortening is a method on the “World Wide Web” in which a URL may be made considerably smaller in length and still lead to the required webpage. This is accomplished by means of an “HTTP Redirect” on a domain name that is short, which links to the webpage that has a long URL. 
-
-If the URL is using Shortening Services, the value assigned to this feature is 1 (phishing) or else 0 (legitimate).
-"""
 
 #listing shortening services
 shortening_services = r"bit\.ly|goo\.gl|shorte\.st|go2l\.ink|x\.co|ow\.ly|t\.co|tinyurl|tr\.im|is\.gd|cli\.gs|" \
@@ -136,12 +82,6 @@ def tinyURL(url):
     else:
         return 0
 
-"""#### **3.1.9. Prefix or Suffix "-" in Domain**
-
-Checking the presence of '-' in the domain part of URL. The dash symbol is rarely used in legitimate URLs. Phishers tend to add prefixes or suffixes separated by (-) to the domain name so that users feel that they are dealing with a legitimate webpage. 
-
-If the URL has '-' symbol in the domain part of the URL, the value assigned to this feature is 1 (phishing) or else 0 (legitimate).
-"""
 
 # 9.Checking for Prefix or Suffix Separated by (-) in the Domain (Prefix/Suffix)
 def prefixSuffix(url):
@@ -150,19 +90,6 @@ def prefixSuffix(url):
     else:
         return 0            # legitimate
 
-"""### **3.2. Domain Based Features:**
-
-Many features can be extracted that come under this category. Out of them, below mentioned were considered for this project.
-
-*   DNS Record
-*   Website Traffic 
-*   Age of Domain
-*   End Period of Domain
-
-Each of these features are explained and the coded below:
-"""
-
-#!pip install python-whois
 
 # importing required packages for this section
 import re
@@ -172,21 +99,10 @@ import urllib
 import urllib.request
 from datetime import datetime
 
-"""#### **3.2.1. DNS Record**
-
-For phishing websites, either the claimed identity is not recognized by the WHOIS database or no records founded for the hostname. 
-If the DNS record is empty or not found then, the value assigned to this feature is 1 (phishing) or else 0 (legitimate).
-"""
 
 # 11.DNS Record availability (DNS_Record)
 # obtained in the featureExtraction function itself
 
-"""#### **3.2.2. Web Traffic**
-
-This feature measures the popularity of the website by determining the number of visitors and the number of pages they visit. However, since phishing websites live for a short period of time, they may not be recognized by the Alexa database (Alexa the Web Information Company., 1996). By reviewing our dataset, we find that in worst scenarios, legitimate websites ranked among the top 100,000. Furthermore, if the domain has no traffic or is not recognized by the Alexa database, it is classified as “Phishing”.
-
-If the rank of the domain < 100000, the vlaue of this feature is 1 (phishing) else 0 (legitimate).
-"""
 
 # 12.Web traffic (Web_Traffic)
 def web_traffic(url):
@@ -202,13 +118,6 @@ def web_traffic(url):
     return 1
   else:
     return 0
-
-"""#### **3.2.3. Age of Domain**
-
-This feature can be extracted from WHOIS database. Most phishing websites live for a short period of time. The minimum age of the legitimate domain is considered to be 12 months for this project. Age here is nothing but different between creation and expiration time.
-
-If age of domain > 12 months, the vlaue of this feature is 1 (phishing) else 0 (legitimate).
-"""
 
 # 13.Survival time of domain: The difference between termination time and creation time (Domain_Age)  
 def domainAge(domain_name):
@@ -232,12 +141,6 @@ def domainAge(domain_name):
       age = 0
   return age
 
-"""#### **3.2.4. End Period of Domain**
-
-This feature can be extracted from WHOIS database. For this feature, the remaining domain time is calculated by finding the different between expiration time & current time. The end period considered for the legitimate domain is 6 months or less  for this project. 
-
-If end period of domain > 6 months, the vlaue of this feature is 1 (phishing) else 0 (legitimate).
-"""
 
 # 14.End time of domain: The difference between termination time and current time (Domain_End) 
 def domainEnd(domain_name):
@@ -260,27 +163,10 @@ def domainEnd(domain_name):
       end = 1
   return end
 
-"""## **3.3. HTML and JavaScript based Features**
-
-Many features can be extracted that come under this category. Out of them, below mentioned were considered for this project.
-
-*   IFrame Redirection
-*   Status Bar Customization
-*   Disabling Right Click
-*   Website Forwarding
-
-Each of these features are explained and the coded below:
-"""
-
 # importing required packages for this section
 import requests
 
-"""### **3.3.1. IFrame Redirection**
 
-IFrame is an HTML tag used to display an additional webpage into one that is currently shown. Phishers can make use of the “iframe” tag and make it invisible i.e. without frame borders. In this regard, phishers make use of the “frameBorder” attribute which causes the browser to render a visual delineation. 
-
-If the iframe is empty or repsonse is not found then, the value assigned to this feature is 1 (phishing) or else 0 (legitimate).
-"""
 
 # 15. IFrame Redirection (iFrame)
 def iframe(response):
@@ -292,12 +178,6 @@ def iframe(response):
       else:
           return 1
 
-"""### **3.3.2. Status Bar Customization**
-
-Phishers may use JavaScript to show a fake URL in the status bar to users. To extract this feature, we must dig-out the webpage source code, particularly the “onMouseOver” event, and check if it makes any changes on the status bar
-
-If the response is empty or onmouseover is found then, the value assigned to this feature is 1 (phishing) or else 0 (legitimate).
-"""
 
 # 16.Checks the effect of mouse over on status bar (Mouse_Over)
 def mouseOver(response): 
@@ -309,12 +189,7 @@ def mouseOver(response):
     else:
       return 0
 
-"""### **3.3.3. Disabling Right Click**
 
-Phishers use JavaScript to disable the right-click function, so that users cannot view and save the webpage source code. This feature is treated exactly as “Using onMouseOver to hide the Link”. Nonetheless, for this feature, we will search for event “event.button==2” in the webpage source code and check if the right click is disabled.
-
-If the response is empty or onmouseover is not found then, the value assigned to this feature is 1 (phishing) or else 0 (legitimate).
-"""
 
 # 17.Checks the status of the right click attribute (Right_Click)
 def rightClick(response):
@@ -326,10 +201,6 @@ def rightClick(response):
     else:
       return 1
 
-"""### **3.3.4. Website Forwarding**
-The fine line that distinguishes phishing websites from legitimate ones is how many times a website has been redirected. In our dataset, we find that legitimate websites have been redirected one time max. On the other hand, phishing websites containing this feature have been redirected at least 4 times.
-"""
-
 # 18.Checks the number of forwardings (Web_Forwards)    
 def forwarding(response):
   if response == "":
@@ -340,10 +211,6 @@ def forwarding(response):
     else:
       return 1
 
-"""## **4. Computing URL Features**
-
-Create a list and a function that calls the other functions and stores all the features of the URL in the list. We will extract the features of each URL and append to this list.
-"""
 
 #Function to extract features
 def featureExtraction(url):
